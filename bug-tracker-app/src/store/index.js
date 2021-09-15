d4cc219f2071b064c162d04b4ccc6b6f141f6ac8
Magesh Kuppan <tkmagesh77@gmail.com>
2021-09-15 20:49:01 +0530

    using redux dev tools

--- bug-tracker-app/src/store/index.js
+++ bug-tracker-app/src/store/index.js
@@ -1,6 +1,7 @@
 import { createStore, combineReducers } from 'redux';
 import bugsReducer from '../bugs/reducers/bugs-reducer';
 import projectsReducer from '../projects/reducers/projects-reducer'
+import { devToolsEnhancer } from 'redux-devtools-extension';
 
 const rootReducer = combineReducers({
     bugsState : bugsReducer,
@@ -9,5 +10,5 @@ const rootReducer = combineReducers({
 //const appStore = createStore(bugsReducer);
 //const appStore = createStore(projectsReducer);
 
-const appStore = createStore(rootReducer);
+const appStore = createStore(rootReducer, devToolsEnhancer());
 export default appStore;
